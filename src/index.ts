4745d79912b499e3bd4b869f87bd1ed40eeb003a
Robbe Van Petegem <robbe@vanpetegem.me>
2021-04-03 21:45:27 +0200

    Watch for a difference in computedStyle to trigger transitio

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,46 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static values = { initial: String };
+  static values = { initial: Boolean, destroy: Boolean };
+  hasDestroyValue: boolean;
   hasInitialValue: boolean;
   observer: MutationObserver;
+  currentDisplayStyle: string;
 
   initialize(): void {
-    this.observer = new MutationObserver(() => {
+    this.observer = new MutationObserver((mutations) => {
       this.observer.disconnect();
-      this.triggerTransition.call(this);
+      this.verifyChange.call(this, mutations);
     });
     if (this.hasInitialValue) this.enter();
     else this.startObserver();
   }
 
-  triggerTransition(): void {
-    (this.element as HTMLElement).hidden ? this.leave() : this.enter();
-  }
-
   async enter(): Promise<void> {
-    (this.element as HTMLElement).hidden = false;
     await this.runTransition("enter");
     this.dispatchEnd("enter");
     this.startObserver();
   }
 
-  async leave(): Promise<void> {
-    (this.element as HTMLElement).hidden = false;
+  async leave(attribute?: string | null): Promise<void> {
+    // Cancel out the display style
+    if (attribute === "hidden") (this.element as HTMLElement).hidden = false;
+    else this.displayStyle = this.currentDisplayStyle;
+
     await this.runTransition("leave");
-    (this.element as HTMLElement).hidden = true;
+
+    // Restore the display style to previous value
+    if (attribute === "hidden") (this.element as HTMLElement).hidden = true;
+    else this.displayStyle = attribute === "style" ? "none" : undefined;
+
     this.dispatchEnd("leave");
-    this.startObserver();
+
+    // Destroy element, or restart observer
+    if (this.hasDestroyValue) this.element.remove();
+    else this.startObserver();
   }
 
-  async destroy(): Promise<void> {
-    await this.leave();
-    this.dispatchEnd("destroy");
-    this.element.remove();
-  }
-
-  // Private functions
-  private startObserver(): void {
-    if (this.element.isConnected)
-      this.observer.observe(this.element, { attributeFilter: ["hidden"] });
-  }
-
+  // Helpers for transition
   private nextFrame(): Promise<number> {
     return new Promise((resolve) => {
       requestAnimationFrame(() => {
@@ -93,10 +89,43 @@ export default class extends Controller {
     );
   }
 
-  private dispatchEnd(name: "enter" | "leave" | "destroy") {
-    const type = `transition:end-${name}`;
+  private dispatchEnd(dir: "enter" | "leave") {
+    const type = `transition:end-${dir}`;
     const event = new CustomEvent(type, { bubbles: true, cancelable: true });
     this.element.dispatchEvent(event);
     return event;
+  }
+
+  private get display(): string {
+    return getComputedStyle(this.element)["display"];
+  }
+
+  private set displayStyle(v: string | undefined) {
+    v
+      ? (this.element as HTMLElement).style.setProperty("display", v)
+      : (this.element as HTMLElement).style.removeProperty("display");
+  }
+
+  // Helpers for observer
+  private verifyChange(mutations: MutationRecord[]): void {
+    const newDisplayStyle = this.display;
+
+    // Make sure there is a new computed displayStyle && the it was or will be "none"
+    if (
+      newDisplayStyle !== this.currentDisplayStyle &&
+      (newDisplayStyle === "none" || this.currentDisplayStyle === "none")
+    )
+      newDisplayStyle === "none"
+        ? this.leave(mutations[0].attributeName)
+        : this.enter();
+    else this.startObserver();
+  }
+
+  private startObserver(): void {
+    this.currentDisplayStyle = this.display;
+    if (this.element.isConnected)
+      this.observer.observe(this.element, {
+        attributeFilter: ["class", "hidden", "style"],
+      });
   }
 }
